926675f43b9512704531734aee25f0ca8a2bdbe5
kgsi <kuusai1998@gmail.com>
2021-01-03 10:11:50 +0900

    type追加

--- @types/declaration.d.ts
+++ @types/declaration.d.ts
@@ -1,5 +1,7 @@
+declare module "next/link";
+declare module "next/head";
+
 declare module "path";
 declare module "fs";
 declare module "next-mdx-remote/render-to-string";
 declare module "next-mdx-remote/hydrate";
-declare module "next/link";
